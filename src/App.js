e3ed5c4b6b2e661b1828c3f277512133a6c870c0
Andrey <stepgt@mail.ru>
2021-10-26 14:13:05 +1000

    Added component  AppRouter

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import About from "./pages/About";
-import Posts from "./pages/Posts";
-import Error from "./pages/Error";
+import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/posts">
-          <Posts />
-        </Route>
-        <Route path="/error">
-          <Error />
-        </Route>
-        <Redirect to="/error" />
-      </Switch>
+      <AppRouter />
     </BrowserRouter>
   );
 }
