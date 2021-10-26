801d9ea8ff13725e0c583a0105f0940befe3cb21
Andrey <stepgt@mail.ru>
2021-10-26 12:59:26 +1000

    Added component Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Route path="/about">
         <About />
       </Route>
