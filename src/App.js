9725c06e10f4e0727c3acf0d55f026a9962a60c7
Andrey <stepgt@mail.ru>
2021-10-26 14:08:25 +1000

    Added component Error

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import About from "./pages/About";
 import Posts from "./pages/Posts";
+import Error from "./pages/Error";
 import Navbar from "./components/UI/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Route path="/about">
-        <About />
-      </Route>
-      <Route path="/posts">
-        <Posts />
-      </Route>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/posts">
+          <Posts />
+        </Route>
+        <Route path="/error">
+          <Error />
+        </Route>
+        <Redirect to="/error" />
+      </Switch>
     </BrowserRouter>
   );
 }
